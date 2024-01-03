262ea7353deb925fa7360a093e981c9fc3d7a890
Mehdi Key <formk@kermed.fr>
2024-01-03 12:27:33 +0100

    Ajout randomcolors

--- index.php
+++ index.php
@@ -2,4 +2,25 @@
 
 require __DIR__.'/vendor/autoload.php';
 
+//composer require mistic100/randomcolor
 
+use \Colors\RandomColor;
+
+// echo RandomColor::one(); 
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Composer</title>
+    <style>
+        .colAl {
+            color:<?=RandomColor::one()?>
+        }
+    </style>
+</head>
+<body>
+<h1 class="colAl">Un titre avec une couleur aléatoire</h1>
+</body>
+</html>
